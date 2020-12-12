417f0a9c78087e8bf4ec75b5f2af653672b08edb
AngelJadanups <ajadanc@est.ups.edu.ec>
2020-12-12 15:54:05 -0500

    creacion paquete y classes Gestion

---
+++ sistemafinanciero/src/main/java/ec/ups/edu/sistemafinanciero/gestion/GestionClienteON.java
@@ -0,0 +1,13 @@
+package ec.ups.edu.sistemafinanciero.gestion;
+
+public class GestionClienteON {
+	public boolean saveCliente() {
+		return true;
+	}
+	public int generarCta() {
+		return 0;
+	}
+	public boolean saveEstCta() {
+		return true;
+	}
+}
---
+++ sistemafinanciero/src/main/java/ec/ups/edu/sistemafinanciero/gestion/GestionInteresON.java
@@ -0,0 +1,23 @@
+package ec.ups.edu.sistemafinanciero.gestion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestionInteresON {
+	public boolean saveIntereses() {
+		return true;
+	}
+	public String searchIntereses() {
+		return "";
+	}
+	public double callInteres() {
+		return 0.00;
+	}
+	public boolean deleteInteres() {
+		return true;
+	}
+	public List<Object> listInteres(){
+		List<Object> lista = new ArrayList<Object>();
+		return lista;
+	}
+}
